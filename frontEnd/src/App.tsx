865b27e3bb9f3dd947c8745bddf0a09e23a6c39b
eduCosta <costaeduardomaciel@gmail.com>
2025-05-08 20:33:43 -0300

    chore: setup initial frontEnd configuration

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl p-8">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontEnd/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {}, // aqui você pode customizar cores, fontes, etc
+  },
+  plugins: [],
+}
+
